42034e7b1034c0edbf81b9554e65c3c8bc86e2f2
Naohiro Tsuji <naohiro2g@code2create.club>
2019-02-06 17:26:21 +0900

    Set trigger=23 and echo=24 for distance sensor. Print distance in cm, rounded down to the first decimal place.

--- 07-distance-sensor-01.py
+++ 07-distance-sensor-01.py
@@ -2,7 +2,7 @@ from gpiozero import DistanceSensor
 from time import sleep
 from decimal import Decimal, ROUND_HALF_UP, ROUND_HALF_EVEN
 
-sensor = DistanceSensor(23, 24)
+sensor = DistanceSensor(echo=24, trigger=23)
 while True:
 #    print('Distance to nearest object is', sensor.distance * 100, 'cm')
 #    print('Distance to nearest object is', '{:.2f}'.format(sensor.distance * 100), 'cm')
