3e7d13621e8f5134578d109b9330a45e7c08b6be
Naohiro Tsuji <naohiro2g@code2create.club>
2019-02-09 02:39:39 +0900

    update

--- 07-distance-sensor-01.py
+++ 07-distance-sensor-01.py
@@ -1,8 +1,15 @@
 from gpiozero import DistanceSensor
 from time import sleep
-from decimal import Decimal, ROUND_HALF_UP, ROUND_HALF_EVEN
+from decimal import Decimal, ROUND_HALF_UP
+import sys
 
 sensor = DistanceSensor(echo=24, trigger=23)
+
+print('trigger is configured to',sensor.trigger)
+print('echo is configured to',sensor.echo)
+
+#    sys.exit()
+
 while True:
 #    print('Distance to nearest object is', sensor.distance * 100, 'cm')
 #    print('Distance to nearest object is', '{:.2f}'.format(sensor.distance * 100), 'cm')
