c4e18d72efdf79dab2a305c13ec072e117203ce4
Naohiro Tsuji <naohiro2g@code2create.club>
2019-02-20 10:48:30 +0900

    update

--- 06-light-sensor-01.py
+++ 06-light-sensor-01.py
@@ -1,6 +1,7 @@
 from gpiozero import LightSensor
 
-ldr1 = LightSensor(11, charge_time_limit=0.001, threshold=0.2)
+# 100nF Capacitor and 1M Ohm CdS Cell
+ldr1 = LightSensor(11, queue_len=10, charge_time_limit=0.001, threshold=0.2)
 
 while True:
     ldr1.wait_for_light()
